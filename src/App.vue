99a260745d7c492611b9ffde04d95f04d48ffb1c
Leila <nb.leila10@gmail.com>
2020-04-03 20:26:43 -0500

    Implement QuoteGrid and Quote components and display them

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <h1>Wonderful Quotes</h1>
+  <div>
+    <h1>Wonderful Quotes</h1>
+    <QuoteGrid :quotes="quotes" />
+  </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10,
+    };
+  },
+  components: {
+    QuoteGrid,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div v-for="quote in quotes" :key="quote">
+      <Quote>{{ quote }}</Quote>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+  components: {
+    Quote,
+  },
+};
+</script>
+
+<style></style>
